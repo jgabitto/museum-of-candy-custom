8a42e7f6ba385a47e1cbe9142edc22ab7c910823
Jorge Gabitto <jgabitto1792@gmail.com>
2020-08-18 17:15:14 -0500

    Hardcoded app.config file

--- src/com/museumofcandy/config/AppConfig.java
+++ src/com/museumofcandy/config/AppConfig.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -29,13 +30,37 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan(basePackages="com.museumofcandy")
-@PropertySources({ 
-	@PropertySource("classpath:persistence-mysql.properties")
-})
+//@PropertySources({ 
+//	@PropertySource("classpath:persistence-mysql.properties")
+//})
 public class AppConfig implements WebMvcConfigurer {
+	@Value( "${jdbc.driver:com.mysql.cj.jdbc.Driver}" )
+	private String jdbcDriver;
+	@Value( "${jdbc.url:mysql://be2f017d4df860:507f06b6@us-cdbr-east-05.cleardb.net/heroku_ed64417cc114036?reconnect=true}" )
+	private String jdbcUrl;
+	@Value( "${jdbc.user:be2f017d4df860}" )
+	private String jdbcUser;
+	@Value( "${jdbc.password:507f06b6}" )
+	private String jdbcPassword;
+	@Value( "${connection.pool.initialPoolSize: 5}")
+	private String connectionPoolInitialPoolSize;
+	@Value( "${connection.pool.minPoolSize: 5}")
+	private String connectionPoolMinPoolSize;
+	@Value( "${connection.pool.maxPoolSize: 20}")
+	private String connectionPoolMaxPoolSize;
+	@Value( "${connection.pool.maxIdleTime: 3000}")
+	private String connectionPoolMaxIdleTime;
+	@Value( "${hibernate.dialect: org.hibernate.dialect.MySQLDialect}")
+	private String hibernateDialect;
+	@Value( "${hibernate.show_sql: true}")
+	private String hibernateShowSql;
+	@Value( "${hibernate.packagesToScan: com.museumofcandy.entity}")
+	private String hibernatePackagesToScan;
 	
-	@Autowired
-	private Environment env;
+
+	
+//	@Autowired
+//	private Environment env;
 	
 	private Logger logger = Logger.getLogger(getClass().getName());
 
@@ -55,7 +80,7 @@ public class AppConfig implements WebMvcConfigurer {
 	        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 
 	    }
-	
+	 
 	@Bean
 	public DataSource myDataSource() {
 				
@@ -64,25 +89,39 @@ public class AppConfig implements WebMvcConfigurer {
 		
 		// set the jdbc driver
 		try {
-			myDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+//			myDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+			myDataSource.setDriverClass(this.jdbcDriver);
 		} catch (PropertyVetoException exc) {
 			throw new RuntimeException(exc);
 		}
 		
 		// log the connection props
-		logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
-		logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+//		logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+//		logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+		logger.info(">>> jdbc.url=" + this.jdbcUrl);
+		logger.info(">>> jdbc.user=" + this.jdbcUser);
 		
 		// set database connection props
-		myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
-		myDataSource.setUser(env.getProperty("jdbc.user"));
-		myDataSource.setPassword(env.getProperty("jdbc.password"));
+		myDataSource.setJdbcUrl(this.jdbcUrl);
+		myDataSource.setUser(this.jdbcUser);
+		myDataSource.setPassword(this.jdbcPassword);
+//		myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+//		myDataSource.setUser(env.getProperty("jdbc.user"));
+//		myDataSource.setPassword(env.getProperty("jdbc.password"));
 		
 		// set connection pool props
-		myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
-		myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
-		myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
-		myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+//		myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+//		myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+//		myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+//		myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+//		myDataSource.setInitialPoolSize(getIntProperty(this.connectionPoolInitialPoolSize));
+//		myDataSource.setMinPoolSize(getIntProperty(this.connectionPoolMinPoolSize));
+//		myDataSource.setMaxPoolSize(getIntProperty(this.connectionPoolMaxPoolSize));
+//		myDataSource.setMaxIdleTime(getIntProperty(this.connectionPoolMaxIdleTime));
+		myDataSource.setInitialPoolSize(5);
+		myDataSource.setMinPoolSize(5);
+		myDataSource.setMaxPoolSize(20);
+		myDataSource.setMaxIdleTime(3000);
 		
 		return myDataSource;
 	}
@@ -92,21 +131,23 @@ public class AppConfig implements WebMvcConfigurer {
 		// set hibernate properties
 		Properties props = new Properties();
 
-		props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-		props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+//		props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+//		props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+		props.setProperty("hibernate.dialect", this.hibernateDialect);
+		props.setProperty("hibernate.show_sql", this.hibernateShowSql);
 		
 		return props;				
 	}
 	
-	private int getIntProperty(String propName) {
-		
-		String propVal = env.getProperty(propName);
-		
-		// now convert to int
-		int intPropVal = Integer.parseInt(propVal);
-		
-		return intPropVal;
-	}
+//	private int getIntProperty(String propName) {
+//		
+//		String propVal = env.getProperty(propName);
+//		
+//		// now convert to int
+//		int intPropVal = Integer.parseInt(propVal);
+//		
+//		return intPropVal;
+//	}
 	
 	@Bean
 	public LocalSessionFactoryBean sessionFactory() {
@@ -116,7 +157,8 @@ public class AppConfig implements WebMvcConfigurer {
 
 		// set the properties
 		sessionFactory.setDataSource(myDataSource());
-		sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+//		sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+		sessionFactory.setPackagesToScan(this.hibernatePackagesToScan);
 		sessionFactory.setHibernateProperties(getHibernateProperties());
 		
 		return sessionFactory;
